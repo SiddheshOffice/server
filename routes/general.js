6e342c9163f39b4b9ec697b9595aedc13c465fd7
SiddheshOffice <162693260+SiddheshOffice@users.noreply.github.com>
2024-03-19 23:30:38 +0530

    adding find all

--- routes/general.js
+++ routes/general.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import {getUser} from "../controllers/general.js";
+import {getUser, getAllUser} from "../controllers/general.js";
 
 
 const router = express.Router();
 router.get("/user/:id", getUser);
-router.get("/general/getAll")
+router.get("/general/getAll", getAllUser)
 
 export default router;
