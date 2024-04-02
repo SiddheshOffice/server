882d34b42d7b88a04ecc622fa0dca699d4b14115
Siddhesh Bhor <162693260+SiddheshOffice@users.noreply.github.com>
2024-04-02 11:17:47 +0000

    c

--- controllers/client.js
+++ controllers/client.js
@@ -1,9 +1,8 @@
-const getCountryISO3 = require("country-iso-2-to-3");
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import Transaction from "../models/Transaction.js";
 import User from "../models/User.js";
-
+import getCountryISO3 from "country-iso-2-to-3";
 
 export const getProducts = async (req, res) => {
   try {
@@ -34,14 +33,13 @@ export const getCustomers = async (req, res) => {
   }
 };
 
-
 export const getTransactions = async (req, res) => {
   try {
     const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
     const generateSort = () => {
       const sortParsed = JSON.parse(sort);
       const sortFormatted = {
-        [sortParsed.field]: sortParsed.sort = "asc" ? 1 : -1,
+        [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
       };
       return sortFormatted;
     };
@@ -53,37 +51,39 @@ export const getTransactions = async (req, res) => {
         { cost: { $regex: new RegExp(search, "i") } },
         { userId: { $regex: new RegExp(search, "i") } },
       ],
-    }).sort(sortFormatted).skip(page * pageSize).limit(pageSize);
+    })
+      .sort(sortFormatted)
+      .skip(page * pageSize)
+      .limit(pageSize);
 
     const total = await Transaction.countDocuments({});
     res.json({
       transactions,
-      total
+      total,
     });
   } catch (error) {
     res.json(error);
   }
 };
 
-
-export const getGeography = async (req, res)=>{
+export const getGeography = async (req, res) => {
   try {
     const users = User.find();
-    const mappedLocations = users.reduce((acc, {country})=> {
+    const mappedLocations = users.reduce((acc, { country }) => {
       const countryISO3 = getCountryISO3(country);
       if (!acc[countryISO3]) {
         acc[countryISO3] = 0;
       }
       acc[countryISO3]++;
       return acc;
-    }, {})
+    }, {});
     const formattedLocations = Object.entries(mappedLocations).map(
-      ([country, count])=> {
-        return {id: country, value: count}
-      }
-    )
-    res.json(formattedLocations)
+      ([country, count]) => {
+        return { id: country, value: count };
+      },
+    );
+    res.json(formattedLocations);
   } catch (error) {
     res.json(error);
   }
-}
+};
