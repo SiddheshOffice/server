34a4ae4478f3d18edbb5056d16e6cbfadbad693c
Siddhesh Bhor <162693260+SiddheshOffice@users.noreply.github.com>
2024-04-10 09:40:36 +0000

    overall stat

--- index.js
+++ index.js
@@ -12,8 +12,15 @@ import salesRoutes from "./routes/sales.js";
 import User from "./models/User.js";
 import Product from "./models/Product.js";
 import ProductStat from "./models/ProductStat.js";
-import {dataUser, dataProduct, dataProductStat, dataTransaction} from "./data/index.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+} from "./data/index.js";
 import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
 // import ip from 'ip';
 
 /* CONFIGURATIONS */
@@ -49,6 +56,7 @@ mongoose
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
     // Transaction.insertMany(dataTransaction);
+    OverallStat.insertMany(dataOverallStat);
   })
   .catch((error) => console.log(`${error} could not connect`));
 
