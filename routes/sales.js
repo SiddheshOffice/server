a00b437b6ca4a1489135f86e9c28ed6f8f845901
Siddhesh Bhor <162693260+SiddheshOffice@users.noreply.github.com>
2024-04-10 10:05:42 +0000

    sales

--- routes/sales.js
+++ routes/sales.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getSales } from "../controllers/sales";
+import { getSales } from "../controllers/sales.js";
 
 const salesRoutes = express.Router();
 salesRoutes.get("/sales", getSales);
